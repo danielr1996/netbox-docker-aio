bb2fefa7a5992a23cb6e539022f48e7ba29ee347
Daniel Richter <me@danielrichter.codes>
2025-07-22 00:54:06 +0200

    fix: remove service plugin
    Signed-off-by: Daniel Richter <me@danielrichter.codes>

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -3,7 +3,15 @@
 
 # To learn how to build images with your required plugins
 # See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
-PLUGINS = ["netbox_floorplan","netbox_topology_views","netbox_prometheus_sd","netbox_qrcode","netbox_reorder_rack","nb_service","netbox_data_flows"]
+PLUGINS = [
+    "netbox_floorplan",
+    "netbox_topology_views",
+    "netbox_prometheus_sd",
+    "netbox_qrcode",
+    "netbox_reorder_rack",
+    # "nb_service",
+    "netbox_data_flows"
+]
 
 PLUGINS_CONFIG = {
   "netbox_topology_views": {
