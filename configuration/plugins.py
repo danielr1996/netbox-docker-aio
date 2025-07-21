3079de1f8ea4e5c37eb088e0469561fe9d71c72e
Daniel Richter <me@danielrichter.codes>
2025-07-21 22:05:06 +0200

    feat: add plugins to netbox image
    Signed-off-by: Daniel Richter <me@danielrichter.codes>

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -3,11 +3,11 @@
 
 # To learn how to build images with your required plugins
 # See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
+PLUGINS = ["netbox_floorplan","netbox_topology_views","netbox_prometheus_sd","netbox_qrcode","netbox_reorder_rack","nb_service","netbox_data_flows"]
 
-# PLUGINS = ["netbox_bgp"]
-
-# PLUGINS_CONFIG = {
-#   "netbox_bgp": {
-#     ADD YOUR SETTINGS HERE
-#   }
-# }
+PLUGINS_CONFIG = {
+  "netbox_topology_views": {
+      "allow_coordinates_saving": True,
+      "always_save_coordinates": True,
+  }
+}
